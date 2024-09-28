6c23fa3bddbd36f5f12769b771c1218af5eba6a7
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-09-28 13:07:24 +0200

    added disable button

--- little-lemon-restaurant/src/App.jsx
+++ little-lemon-restaurant/src/App.jsx
@@ -50,19 +50,23 @@ function App(){
 
   const handleSubmit = (e) =>{
     e.preventDefault();
+    setName("");
+    console.log("Form submitted!");
   }
   return(
     <div className="App">
       <form onSubmit={handleSubmit}>
         <fieldset>
      <div className="Field">
-      <label>Name</label>
-      <input type="text"
+      <label htmlFor="name">Name</label>
+      <input
+      id="name"
+      type="text"
        placeholder="Name" 
        name="name" 
        value={name} onChange={e => setName(e.target)} />
      </div>
-     <button type="submit">Submit</button>
+     <button disabled={!name} type="submit">Submit</button>
         </fieldset>
 
       </form>
