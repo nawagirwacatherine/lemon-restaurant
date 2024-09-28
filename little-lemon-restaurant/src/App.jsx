f5a1150ac2155d9fb079843290fee5ced769fe59
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-09-28 12:33:40 +0200

    have added interactivity in the form

--- little-lemon-restaurant/src/App.jsx
+++ little-lemon-restaurant/src/App.jsx
@@ -1,39 +1,73 @@
-import './style.css'
-function App() {
+// import './style.css'
 
-  return(
+// function App () {
+
+//   return(
   
-    <form className="form-container">
-      <div className="form-field">
-        <label htmlFor="date">Date</label>
-        <input type="text" id="date" />
-      </div>
+//     <form className="form-container">
+//       <div className="form-field">
+//         <label htmlFor="date">Date</label>
+//         <input type="text" id="date" />
+//       </div>
 
-      <div className="form-field"><label htmlFor="time">Time</label>
-      <input type="text" id="time" />
-      </div>
+//       <div className="form-field"><label htmlFor="time">Time</label>
+//       <input type="text" id="time" />
+//       </div>
 
-      <div className="form-field">
-      <label htmlFor="number-of-guests">Number of guests</label>
-      <input type="text" id="number-of-guests" />
-      </div>
+//       <div className="form-field">
+//       <label htmlFor="number-of-guests">Number of guests</label>
+//       <input type="text" id="number-of-guests" />
 
-
-      <div className="form-field">
-      <label htmlFor="occasion">Occasion(Birthday Anniversary)</label>
-      <input type="text" id="occasion" />
-      </div>
+//       </div>
+      
 
 
-      <div className="form-field">
-      <label htmlFor="reservation">Submit reservation button</label>
-      <input type="submit" id="reservation" />
-      </div>
+//       <div className="form-field">
+//       <label htmlFor="occasion">Occasion(Birthday Anniversary)</label>
+//       <input type="text" id="occasion" />
+//       </div>
 
 
-    </form>
-  )
+//       <div className="form-field">
+//       <label htmlFor="reservation">Submit reservation button</label>
+//       <input type="submit" id="reservation" />
+//       </div>
 
+
+//     </form>
+   
+//   )
+
+// }
+
+// export default App
+
+import { useState } from "react";
+
+
+function App(){
+  const[name, setName]= useState("");
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  }
+  return(
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+     <div className="Field">
+      <label>Name</label>
+      <input type="text"
+       placeholder="Name" 
+       name="name" 
+       value={name} onChange={e => setName(e.target)} />
+     </div>
+     <button type="submit">Submit</button>
+        </fieldset>
+
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
