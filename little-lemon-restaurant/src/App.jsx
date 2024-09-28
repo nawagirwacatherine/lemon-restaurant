6798431544658b9cb90567dc0e33a69cd765bef5
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-09-28 08:21:33 +0200

    first commit

---
+++ little-lemon-restaurant/src/App.jsx
@@ -0,0 +1,38 @@
+import './style.css'
+function App() {
+
+  return(
+    <form className="form-container">
+      <div className="form-field">
+        <label htmlFor="date">Date</label>
+        <input type="text" id="date" />
+      </div>
+
+      <div className="form-field"><label htmlFor="time">Time</label>
+      <input type="text" id="time" />
+      </div>
+
+      <div className="form-field">
+      <label htmlFor="number-of-guests">Number of guests</label>
+      <input type="text" id="number-of-guests" />
+      </div>
+
+
+      <div className="form-field">
+      <label htmlFor="occasion">Occasion(Birthday Anniversary)</label>
+      <input type="text" id="occasion" />
+      </div>
+
+
+      <div className="form-field">
+      <label htmlFor="reservation">Submit reservation button</label>
+      <input type="submit" id="reservation" />
+      </div>
+
+
+    </form>
+  )
+
+}
+
+export default App
